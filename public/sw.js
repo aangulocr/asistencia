073b27e81488085d7a0f23d1089d3dbf1f393c29
AAM <aangulocr@gmail.com>
2026-02-22 20:58:13 -0600

    Soporte multianual, PWA setting up

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+// Basic Service Worker for PWA installability
+const CACHE_NAME = 'asistencia-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
